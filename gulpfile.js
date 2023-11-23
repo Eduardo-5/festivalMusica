bf34b9e2d33744c5fc19256db87fc42f50d9edca
Eduardo <ramoseduardo.re5@gmail.com>
2023-11-23 11:32:20 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const { src, dest } = require("gulp");
+const sass = require("gulp-sass");
+
+function compilarSASS() {
+  return src("./src/scss/app.scss").pipe(sass()).pipe(dest("./build/css"));
+}
+
+exports.compilarSASS = compilarSASS;
